d0d4b4dfdcb5e3fc800dc4264247a10cd78bad65
hyeon5 <gusdh2510@gmail.com>
2023-04-10 14:13:22 +0900

    AlertDialog만들고 mainActiviy 수정

--- app/src/main/java/net/skhu/MainActivity.java
+++ app/src/main/java/net/skhu/MainActivity.java
@@ -1,20 +1,26 @@
 package net.skhu;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -31,15 +37,19 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, buttonsActivity.class);
             startActivity(intent);
             return true;
-        }else if (id == R.id.action_checkboxes) {
+        } else if (id == R.id.action_checkboxes) {
             Intent intent = new Intent(this, CheckboxesActivity.class);
             startActivity(intent);
             return true;
-        }else if (id == R.id.action_spinners){
-            Intent intent = new Intent(this,SpinnersActivity.class);
+        } else if (id == R.id.action_spinners) {
+            Intent intent = new Intent(this, SpinnersActivity.class);
             startActivity(intent);
             return true;
-            }
-         return super.onOptionsItemSelected(item);
+        } else if (id == R.id.action_alerts) {
+            Intent intent = new Intent(this, AlertsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
